889fa8412526baa08db60b37a3eb19d49f8b8024
pumanitro <pumanitro@gmail.com>
2017-10-29 20:03:56 +0100

    Connected actual coordinates to the map.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -27,24 +27,4 @@ export class HomePage {
     });
 
   }
-
-  increaseZoom() {
-    this.zoom  = Math.min(this.zoom + 1, 18);
-    console.log('zoom: ', this.zoom);
-  }
-
-  decreaseZoom() {
-    this.zoom  = Math.max(this.zoom - 1, 1);
-    console.log('zoom: ', this.zoom);
-  }
-
-  increaseOpacity() {
-    this.opacity  = Math.min(this.opacity + 0.1, 1);
-    console.log('opacity: ', this.opacity);
-  }
-
-  decreaseOpacity() {
-    this.opacity  = Math.max(this.opacity - 0.1, 0);
-    console.log('opacity: ', this.opacity);
-  }
 }
